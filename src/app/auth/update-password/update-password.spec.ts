10723328fa7b08e981d5df383073966c36689669
xuan-323 <GitHub@example.com>
2025-12-16 16:15:44 +0800

    fix: 修正測試檔的路徑引入

--- src/app/auth/update-password/update-password.spec.ts
+++ src/app/auth/update-password/update-password.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UpdatePasswordComponent } from './update-password.component';
 
+
+
 describe('UpdatePasswordComponent', () => {
   let component: UpdatePasswordComponent;
   let fixture: ComponentFixture<UpdatePasswordComponent>;
